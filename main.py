4a9c731dfbb687b7769b1535d7edac5dee7c5130
Morteza Maleki <maleki.morteza92@gmail.com>
2023-07-20 07:35:53 +0430

    game of bri

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pygame
-
+import random
 pygame.init()
 
 # Red Green Blue
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
-
+green = (0, 255, 0)
 dis = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Maila's Game")
 
@@ -19,7 +19,7 @@ x1_change = 0
 y1_change = 0
 
 clock = pygame.time.Clock()
-
+x2, y2 = 50, 0 
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -43,7 +43,19 @@ while not game_over:
     if x1>800 or x1<0 or y1>600 or y1<0:
         break
 
+
     dis.fill(white)
+    y2 = y2 + 5
+
+    random.randint(0, 800)
+    for x2 in range(0, 800, 200):
+        pygame.draw.rect(dis, red, [x2, y2, 40, 40])
+
+        if x2-40<x1<x2+40 and y2-40<y1<y2+40 :
+            game_over = True
+    if y2 > 600:
+        y2 = 0
+        
     pygame.draw.rect(dis, black, [x1, y1, 40, 40])
 
     pygame.display.update()
