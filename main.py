90748e79a8a6c84640aa6f955cf5b09f5645a2ef
Maila <mrs.maila.yadgarian@gmail.com>
2023-08-06 19:48:43 +0400

    added black ghost

--- main.py
+++ main.py
@@ -10,32 +10,34 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 music = pygame.mixer.music.load('alarm.wav')
 
-dis_x = 1000
-dis_y = 600
+dis_x = 1200
+dis_y = 700
 dis = pygame.display.set_mode((dis_x, dis_y))
 pygame.display.set_caption("Maila's Game")
 
 game_over = False
 
-x1 = 300
-y1 = 300
+xghost = 300
+yghost = 300
 
 
-x1_change = 0        
-y1_change = 0
+xghost_change = 0        
+yghost_change = 0
 
 size_x = 100
 size_y = 100
-
-obstacles = [(50, 100), (200,300), (300, 600), (200,100), (250,100), (300, 100), (400, 100), (500, 100)]
+xgreen = dis_x / 2
+ygreen = dis_y / 2
+obstacles = [(50, 100), (700,300), (550, 750), (200,100), (250,600), (300, 100), (450, 100), (850, 700)]
 
 player_image = pygame.image.load('player.png')  # Load the player image
 player_image = pygame.transform.scale(player_image, (size_x, size_y))  # Scale the image if needed
+black_ghost_image = pygame.image.load('black ghost.png')  # Load the player image
+black_ghost_image = pygame.transform.scale(black_ghost_image, (size_x, size_y))  # Scale the image if needed
 
-
-def fire(dis, x1, y1):
+def fire(dis, xghost, yghost):
     for i in range(10):
-        pygame.draw.rect(dis, red, [x1+random.randint(-50, 90), y1+random.randint(-50, 90), 8, 8])
+        pygame.draw.rect(dis, red, [xghost+random.randint(-50, 90), yghost+random.randint(-50, 90), 8, 8])
 
 def obstacle(dis):
     for obs in obstacles:
@@ -51,51 +53,58 @@ while not game_over:
             game_over = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x1_change = -50
-                y1_change = 0
+                xghost_change = -50
+                yghost_change = 0
             elif event.key == pygame.K_RIGHT:
-                x1_change = 50
-                y1_change = 0
+                xghost_change = 50
+                yghost_change = 0
             elif event.key == pygame.K_UP:
-                y1_change = -50
-                x1_change = 0
+                yghost_change = -50
+                xghost_change = 0
             elif event.key == pygame.K_DOWN:
-                y1_change = 50
-                x1_change = 0
+                yghost_change = 50
+                xghost_change = 0
 
-    x1 = x1 + x1_change
-    y1 = y1 + y1_change
+    xghost = xghost + xghost_change
+    yghost = yghost + yghost_change
 
-    if (x1, y1) in obstacles:
-        break
-    # if x1>dis_x:
-    #     x1=0
+    # if (xghost, yghost) in obstacles:
+    #     break
+    # if xghost>dis_x:
+    #     xghost=0
     #     size_x = size_x + 50
-    # elif x1<0:
+    # elif xghost<0:
     #     size_x=size_x-25
-    #     x1=dis_x
-    # if y1>dis_y:
+    #     xghost=dis_x
+    # if yghost>dis_y:
     #     size_y=size_y+100
-    #     y1=0
-    #     y1 = y1 + 5
-    # elif y1<0:
-    #     y1=dis_y
+    #     yghost=0
+    #     yghost = yghost + 5
+    # elif yghost<0:
+    #     yghost=dis_y
     #     size_y=size_y-5
         
-    dis.fill(white)
-      
- #   pygame.draw.rect(dis, black, [x1, y1, size_x, size_y])
-    dis.blit(player_image, (x1, y1))
+    dis.fill(black)
+    if xghost - xgreen>0:
+        xgreen = xgreen + 10
+    else :
+        xgreen = xgreen - 10
+    if yghost - ygreen>0:
+        ygreen = ygreen + 10
+    else :
+        ygreen = ygreen - 10
+    # pygame.draw.rect(dis, green, [xgreen, ygreen, size_x, size_y])
+    dis.blit(player_image, (xghost, yghost))
+    dis.blit(black_ghost_image, (xgreen, ygreen))
+   # obstacle(dis)
 
-    obstacle(dis)
-
-    if (dis_x - x1)<40 or (dis_y - y1)<40 :
-        fire(dis, x1, y1)
+    # if (dis_x - xghost)<40 or (dis_y - yghost)<40 :
+    #     fire(dis, xghost, yghost)
         #pygame.mixer.music.play(2)
 
     pygame.display.update()
 
-    clock.tick(2)
+    clock.tick(10)
 
 pygame.quit()
 quit()
